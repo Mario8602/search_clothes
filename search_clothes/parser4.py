b8059eb57a837ed6a50a6c010914c655e8b65d54
Mario8602 <hellp8901@gmail.com>
2023-02-06 19:27:27 +0500

    Commit #3 добавлен файл для отдельного вынесения куки и заголовков сайта, исправлены ошибки

--- search_clothes/parser4.py
+++ search_clothes/parser4.py
@@ -1,5 +1,6 @@
 import json
 from time import perf_counter
+import os.path
 
 import requests
 from bs4 import BeautifulSoup as bs
@@ -8,11 +9,11 @@ from site_data.headers_params import cookies, headers, params
 
 
 urls = {
-    "catalog": "/catalog/",
-    "monitory": {
-        "url": "https://www.dns-shop.ru/catalog/17a8943716404e77/monitory/",
-        "tag": "a",
-        "class_": "catalog-product__name ui-link ui-link_black"
+    'catalog': '/catalog/',
+    'monitory': {
+        'url': 'https://www.dns-shop.ru/catalog/17a8c23316404e77/komplekty-klaviaturamysh/',
+        'tag': 'a',
+        'class_': 'catalog-product__name ui-link ui-link_black'
     }}
 
 
@@ -31,24 +32,26 @@ def page_count_search(url):
 
 
 def get_uniq_id(url):
+    if not os.path.exists('json_data'):
+        os.makedirs('json_data')
     number_of_pages = page_count_search(url)
     unique_id = []
     for page in range(1, int(number_of_pages)+1):
-        response = requests.get(url, params={"p": page}, headers=headers, cookies=cookies)
+        response = requests.get(url, params={'p': page}, headers=headers, cookies=cookies)
         data = json.loads(response.text)
         data_bs = bs(data['html'], 'lxml')
         print(f'Поиск уникальных id товара на странице: {page}')
 
-        info_prod = data_bs.find_all("div", class_="catalog-product ui-button-widget")
+        info_prod = data_bs.find_all('div', class_='catalog-product ui-button-widget')
         for i in info_prod:
             unique_id.append(i.get('data-product'))
 
-    with open("json_data/my.txt", "w") as file:
+    with open('json_data/my.txt', 'w') as file:
         file.write(str(unique_id))
 
 
 def get_data_prod():
-    lst_id = open("json_data/my.txt", "r")
+    lst_id = open('json_data/my.txt', 'r')
     lst_id_my = lst_id.read()
     data_items = {}
     item_number = 0
@@ -73,21 +76,21 @@ def get_data_prod():
 
         if len(item_price) > 1:
             data_items[item_id] = {
-                "item_name": item_name,
-                "item_price_old": item_price_old,
-                "item_price_sale": item_price_sale,
+                'item_name': item_name,
+                'item_price_old': item_price_old,
+                'item_price_sale': item_price_sale,
             }
 
-    with open("json_data/5_dns_result.json", "w") as file:
+    with open('json_data/5_dns_result.json', 'w') as file:
         json.dump(data_items, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    get_uniq_id("https://www.dns-shop.ru/catalog/17a8943716404e77/monitory/")
+    get_uniq_id('https://www.dns-shop.ru/catalog/17a8c23316404e77/komplekty-klaviaturamysh/')
     get_data_prod()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     start = perf_counter()
     main()
-    print(f"time: {(perf_counter() - start):.02f}")
+    print(f'time: {(perf_counter() - start):.02f}')
---
+++ search_clothes/site_data/headers_params.py
@@ -0,0 +1,13 @@
+cookies = {
+    """paste you cookies"""
+}
+
+headers = {
+    """paste you headers"""
+}
+
+params = {
+    """paste you params"""
+}
+
+data = """paste you data"""
